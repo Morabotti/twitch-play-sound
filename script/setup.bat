fcd2f52d118b1a7df5f290852cb03adb98dde136
Jesse Batman <jesse.batman@outlook.com>
2019-09-13 01:24:45 +0300

    Fixed setup.bat

--- script/setup.bat
+++ script/setup.bat
@@ -4,9 +4,9 @@ cd ../server
 call npm install
 IF NOT EXIST ".env" (
   echo F | xcopy .env.example .env
-  echo =============================================================================================
-  echo .env file was created. Make sure that you add a mongoDB address or else program will not work.
-  echo =============================================================================================
+  echo ======================
+  echo .env file was created.
+  echo ======================
 )
 npm run production
 @pause
